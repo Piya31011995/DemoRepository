67d9c1b4d79d8c2dbdc5d79439c21cef5d91672c
Piya31011995 <152988673+Piya31011995@users.noreply.github.com>
2023-12-05 20:52:31 +0530

    loginpagecomit

---
+++ TestProject/src/pageLayer/LoginPage.java
@@ -0,0 +1,17 @@
+package pageLayer;
+
+public class LoginPage {
+
+	public static void main(String[] args) {
+System.out.println("log in page test cases");
+System.out.println("log in page test cases");
+System.out.println("log in page test cases");
+System.out.println("2");
+System.out.println("3");
+
+
+
+
+	}
+
+}
